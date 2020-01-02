47c714bc739e47ea866f6b6b975ac477b21d99c9
Adam Parker <adam3692@hotmail.co.uk>
2020-01-02 12:04:21 +0000

    test

---
+++ index.php
@@ -0,0 +1,49 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Grid</title>
+
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+
+    <section class="header">
+        <img src="https://dummyimage.com/250x100/000/fff" alt="Buildcraft Logo">
+        
+        <div class="contact">
+          <p>Call us: 07828 882 882</p>
+          <a href="#" class="btn">Contact us</a>
+        </div>
+      </section>
+      
+      <nav>
+        <ul>
+          <li><a href="#" class="vertical">HOME</a></li>
+          <li><a href="#">ABOUT</a></li>
+          <li><a href="#">CONTACT</a></li>
+          <li><a href="#" class="last">SERVICES</a></li>
+        </ul>
+      </nav>
+      
+      <section class="main" id="wrapper">
+        <h1 class="title">Our Company<div class="underline"></div></h1>
+        <div class="grid">
+          
+          <p class="grid-item text bold orange grid-left-top">In 1990 Buildcraft Landscaping was first established as a two man business. It has grown rapidly via local advertising, recommendations and word of mouth.</p>
+          <img src="https://dummyimage.com/700x300/000/fff" alt="Content image" class="grid-item image grid-right-top">
+          
+          <img src="https://dummyimage.com/700x300/000/fff" alt="Content image" class="grid-item image grid-left-mid">
+          <p class="grid-item text green roboto-light grid-right-mid">Our company continues to build on our reputation through expanding our portfolio of services. We work with commercial and private clients, architects and garden designers throughout Surrey and South London , with our broad range of services that we offer has expanded beyond what even our expectations were.</p>
+          
+          <p class="grid-item text orange roboto-light ml0 grid-left-bottom">Buildcraft Landscaping, in just a few years on, has doubled its permanent staff and during the peak seasons hired additional workers. Investing in specialist equipment and customised vehicles show that Buildcraft Landscaping is a true gardening force to contend with!</p>
+          <img src="https://dummyimage.com/700x300/000/fff" class="grid-item image grid-right-bottom" alt="Content Image">
+        </div>
+        <p class="text quote orange">We promise to provide you with the highest quality, comprehensive service
+          that covers all aspects of landscape maintenance and construction. <p class="green"><b>Joseph</b> - <em>Company Owner</em></p></p>
+      </section>
+
+
+</body>
+</html>
