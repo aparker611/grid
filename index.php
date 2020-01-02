b2f2b44073df6134f4572e969af683c1c1489c03
Adam Parker <adam3692@hotmail.co.uk>
2020-01-02 17:21:11 +0000

    Added content security policy

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Security-Policy" 
-      content="default-src 'self' data: gap: 'unsafe-eval ws: ;
+      content="default-src 'self' data: gap: 'unsafe-eval' ws: ;
                style-src * 'unsafe-inline';
                script-src *;
                media-src *;
